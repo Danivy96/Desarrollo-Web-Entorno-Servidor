e95b3ea83832a05d3c31dee227a7c9491f83527d
Danivy96 <danivy96@gmail.com>
2026-09-22 12:51:10 +0200

    Inicio ejercicio 4

--- ej2_strings.php
+++ ej2_strings.php
@@ -1,25 +1,30 @@
 <HTML>
-<HEAD><TITLE> EJ2 Strings - Analizador de nombre de usuario </TITLE></HEAD>
+
+<HEAD>
+    <TITLE> EJ2 Strings - Analizador de nombre de usuario </TITLE>
+</HEAD>
+
 <BODY>
-<?php
- $nombre = " aLBeRTo gaRCia loPEz <br>";
- echo "Cadena original: " .$nombre;
+    <?php
+    $nombre = " aLBeRTo gaRCia loPEz <br>";
+    echo "Cadena original: " . $nombre;
 
- $nombreMin = strtolower($nombre);
- echo "Nombre normalizado: " .ucwords($nombreMin);
+    $nombreMin = strtolower($nombre);
+    echo "Nombre normalizado: " . ucwords($nombreMin);
 
- $nombreNoEspacios = str_replace(" ", "",$nombre);
- echo "Número de carácteres: " .strlen($nombreNoEspacios)."<br>";
+    $nombreNoEspacios = str_replace(" ", "", $nombre);
+    echo "Número de carácteres: " . strlen($nombreNoEspacios) . "<br>";
 
- $nombreArray= explode(" ", $nombreMin);
- echo "Nombre: " .ucwords($nombreArray[1])."<br>";
- echo "Primer apellido: " .ucwords($nombreArray[2])."<br>";
- echo "Segundo apellido: " .ucwords($nombreArray[3])."<br>";
+    $nombreArray = explode(" ", $nombreMin);
+    echo "Nombre: " . ucwords($nombreArray[1]) . "<br>";
+    echo "Primer apellido: " . ucwords($nombreArray[2]) . "<br>";
+    echo "Segundo apellido: " . ucwords($nombreArray[3]) . "<br>";
 
- echo "Iniciales: " .ucwords(substr($nombreArray[1],0,1))."." .ucwords(substr($nombreArray[2],0,1)).".".ucwords(substr($nombreArray[3],0,1)).". <br>";
+    echo "Iniciales: " . ucwords(substr($nombreArray[1], 0, 1)) . "." . ucwords(substr($nombreArray[2], 0, 1)) . "." . ucwords(substr($nombreArray[3], 0, 1)) . ". <br>";
 
- echo "Nombre de usuario: ". $nombreArray[1].".".$nombreArray[2];
+    echo "Nombre de usuario: " . $nombreArray[1] . "." . $nombreArray[2];
 
-?>
+    ?>
 </BODY>
+
 </HTML>
--- ej3_strings.php
+++ ej3_strings.php
@@ -1,23 +1,49 @@
 <HTML>
-<HEAD><TITLE> EJ3 Strings - Analizador de correo electrónico </TITLE></HEAD>
+
+<HEAD>
+    <TITLE> EJ3 Strings - Analizador de correo electrónico </TITLE>
+</HEAD>
+
 <BODY>
-<?php
- $email = "alberto.garcia@educa.madrid.org";
- echo "Email: " .$email."<br>";
+    <?php
+    $email = "alberto.garcia@educa.madrid.org";
+    echo "Email: " . $email . "<br>";
 
- $posArroba = strpos($email,"@");
- $tieneArroba = $posArroba == true;
+    $posArroba = strpos($email, "@");
+    $tieneArroba = $posArroba == true;
+    $terminaEnOrg = str_ends_with($email, ".org");
 
- $emailArray = explode("@", $email);
- $usuario = $emailArray[0];
- echo "Usuario: " .$usuario ."<br>";
 
- $dominio = $emailArray[1];
- echo "Dominio: ". $dominio ."<br>";
- 
-$dominioArray = explode(".", $dominio);
-echo "Organización: " .$dominioArray[0] ."<br>";
-echo "Extensión: " .$dominioArray[2] ."<br>";
-?>
+    $emailArray = explode("@", $email);
+    $usuario = $emailArray[0];
+    echo "Usuario: " . $usuario . "<br>";
+
+    $dominio = $emailArray[1];
+    echo "Dominio: " . $dominio . "<br>";
+
+    $dominioArray = explode(".", $dominio);
+    echo "Organización: " . $dominioArray[0] . "<br>";
+    echo "Extensión: " . $dominioArray[2] . "<br>";
+    echo "<br>";
+
+    $usuarioLength = strlen($usuario);
+    $dominioLength = strlen($dominio);
+
+    echo "El usuario contiene " . $usuarioLength . " caracteres <br>";
+    echo "El dominio contiene " . $dominioLength . " caracteres <br>";
+
+    if ($tieneArroba) {
+        echo "El email contiene el arroba <br>";
+    } else {
+        echo "El email NO contiene el arroba <br>";
+    }
+
+    if ($terminaEnOrg) {
+        echo " El dominio temina en .org <br>";
+    } else {
+        echo "El dominio NO termina en .org <br>";
+    }
+    ?>
 </BODY>
+
 </HTML>
