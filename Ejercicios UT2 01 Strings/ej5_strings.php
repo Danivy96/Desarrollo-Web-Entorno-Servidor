4200fda6ac05ef680d083623945207b6fd17db52
Danivy96 <danivy96@gmail.com>
2026-09-25 10:48:17 +0200

    Inicio ejercicios bucles

--- Ejercicios UT2 01 Strings/ej5_strings.php
+++ Ejercicios UT2 01 Strings/ej5_strings.php
@@ -3,6 +3,9 @@
 <BODY>
 <?php
  $url = "https://www.tienda.es/productos/portatil.php?id=34&marca=lenovo";
+
+ $posHttp = strpos($url,"w");
+ $sinHttp = substr($url,0, $posHttp);
 ?>
 </BODY>
 </HTML>
---
+++ Ejercicios UT2 02 Bucles/ej1_bucles.php
@@ -0,0 +1,9 @@
+<HTML>
+<HEAD><TITLE> EJ1 Bucles – Estadística secuencia </TITLE></HEAD>
+<BODY>
+<?php
+ $inicio = 1;
+ $fin = 100;
+?>
+</BODY>
+</HTML>
